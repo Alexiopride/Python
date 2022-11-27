f0661fba0749f97e64a240eddd8ac02400eedca4
alexiopride <alexiopride@gmail.com>
2022-11-27 16:56:33 +0400

    Added task 18

---
+++ HW2/Task_18.py
@@ -0,0 +1,16 @@
+# Реализуйте алгоритм перемешивания списка(shuffle использовать нельзя, другие методы из библиотеки random - можно).
+
+from random import randrange
+
+def sattolo_cycle(items):
+    i = len(items)
+    while i > 1:
+        i = i - 1
+        j = randrange(i)  # 0 <= j <= i-1
+        items[j], items[i] = items[i], items[j]
+    return items
+
+
+list = [1, 2, 3, 4, 5]
+print(f'Начальный список: {list}.')
+print(f'Перемешанный список: {sattolo_cycle(list)}.')
